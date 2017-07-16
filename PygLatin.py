b68d29cfa1a5d30f69b6b15462ddf34bafa58560
JMAR <529Design@users.noreply.github.com>
2017-07-15 21:06:12 -0400

    Update PygLatin.py

--- PygLatin.py
+++ PygLatin.py
@@ -6,3 +6,18 @@ if word.isalpha():
     print word
 else:
     print "invalid entry"
+
+    #alternate code:
+    """
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    new_word = word[1:] + first + pyg
+    print new_word
+else:
+    print 'empty'
+    """
